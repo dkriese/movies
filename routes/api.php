f296bcb7731a274ca9d1e8f348c4bf9efa186fb8
Diana K <dkriese@gmail.com>
2018-09-28 13:40:04 -0600

    modified routes files

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Resources\MovieResource;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('movies', function() {
+    return DB::select('select * from movies');
 });
